205f17ec13cc94967a5dbcb264d2ad52e8c5283b
hanahanaside <flower@flower-no-MacBook-Air.local>
2014-08-03 11:28:59 +0900

    ルームアイテム購入完了

---
+++ Assets/Mansion/Scripts/Main/BuyRoomItemDialogController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class BuyRoomItemDialogController : MonoBehaviour {
+
+	public static event Action itemBoughtEvent;
+	public static event Action dialogClosedEvent;
+
+	public void Init (RoomData roomData) {
+
+	}
+
+	public void OnBuyButtonClicked () {
+		itemBoughtEvent ();
+		Destroy (transform.parent.gameObject);
+	}
+
+	public void OnCloseButonClicked () {
+		dialogClosedEvent ();
+		Destroy (transform.parent.gameObject);
+	}
+}
--- Assets/Mansion/Scripts/Main/RoomController.cs
+++ Assets/Mansion/Scripts/Main/RoomController.cs
@@ -3,7 +3,38 @@ using System.Collections;
 
 public class RoomController : MonoBehaviour {
 
+	public GameObject buyButton;
+	public GameObject uiRoot;
+	public GameObject buyRoomItemDialogPrefab;
+	
 	public void OnBuyButtonClicked () {
+		ShowBuyRoomItemDialog ();
+		iTweenEvent removeEvent = iTweenEvent.GetEvent (buyButton, "ExitEvent");
+		removeEvent.Play ();
+	}
 
+	private void ShowBuyRoomItemDialog () {
+		GameObject buyRoomItemDialogObject = Instantiate (buyRoomItemDialogPrefab) as GameObject;
+		buyRoomItemDialogObject.transform.parent = uiRoot.transform;
+		buyRoomItemDialogObject.transform.localScale = new Vector3 (1, 1, 1);
+		BuyRoomItemDialogController.itemBoughtEvent += itemBoughtEvent;
+		BuyRoomItemDialogController.dialogClosedEvent += dialogClosedEvent;
+	}
+
+	public void itemBoughtEvent () {
+		Debug.Log ("boughtEvent");
+		BuyRoomItemDialogController.itemBoughtEvent -= itemBoughtEvent;
+		PlayComeBackEvent ();
+	}
+
+	public void dialogClosedEvent () {
+		Debug.Log ("closedEvent");
+		BuyRoomItemDialogController.dialogClosedEvent -= dialogClosedEvent;
+		PlayComeBackEvent ();
+	}
+
+	private void PlayComeBackEvent () {
+		iTweenEvent comeBackEvent = iTweenEvent.GetEvent (buyButton, "ComeBackEvent");
+		comeBackEvent.Play ();
 	}
 }
