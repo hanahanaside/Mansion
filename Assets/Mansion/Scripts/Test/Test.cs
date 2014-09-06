ea0aec2dfb995ac4cda1a9238366bfa02420a96d
hanahanaside <flower@flower-no-MacBook-Air.local>
2014-09-06 21:24:58 +0900

    失敗

--- Assets/Mansion/Scripts/Test/Test.cs
+++ Assets/Mansion/Scripts/Test/Test.cs
@@ -5,4 +5,26 @@ using System.Collections.Generic;
 
 public class Test : MonoBehaviour {
 
+	private string url = "http://www.yahoo.co.jp/";
+
+	void Start(){
+
+	}
+
+	public void Open(){
+		Debug.Log ("open");
+		EtceteraBinding.inlineWebViewShow( 50, 10, 260, 300 );
+		EtceteraBinding.inlineWebViewSetUrl( "http://google.com" );
+
+	}
+
+	public void Close(){
+		Debug.Log ("close");
+		EtceteraBinding.inlineWebViewClose ();
+	}
+
+	public void Open2(){
+		EtceteraBinding.inlineWebViewShow( 50, 300, 260, 300 );
+		EtceteraBinding.inlineWebViewSetUrl(url );
+	}
 }
