912a4bf70c0d6231b366ce0260017dbdc379baba
hanahanaside <flower@flower-no-MacBook-Air.local>
2014-10-26 13:41:14 +0900

    Adcrops対応を開始

---
+++ Assets/Plugins/Android/PushmanManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PushmanManager : MonoBehaviour {
+	private AndroidJavaObject mActivity;
+	private AndroidJavaClass mPushman;
+
+	void Start () {
+		AndroidJavaClass player = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");   
+		mActivity = player.GetStatic<AndroidJavaObject> ("currentActivity");
+		mActivity.Call ("runOnUiThread", new AndroidJavaRunnable (() => {
+			mPushman = new AndroidJavaClass ("jp.pushman.android.PushmanSDK");
+			mPushman.CallStatic ("receiveOnCreate", mActivity);
+		}));
+	}
+
+	void OnApplicationPause (bool pauseStatus) {
+		Debug.Log ("pause");
+		if (pauseStatus) {
+			mPushman.CallStatic ("stop", mActivity);
+		} else {
+			mPushman.CallStatic ("start", mActivity);
+		}
+	}
+}
